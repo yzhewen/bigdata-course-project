e40532b86b48fc302328e32a01775a5dc8790f1a
yzhewen <58250109+yzhewen@users.noreply.github.com>
2019-12-22 14:27:30 +0800

    Add files via upload

---
+++ task03/run0303.sh
@@ -0,0 +1,5 @@
+hadoop jar /home/uic/hadoop-3.2.1/share/hadoop/tools/lib/hadoop-*streaming*.jar \
+-file /home/uic/bd-project/task03/mapper0303.py -mapper /home/uic/bd-project/task03/mapper0303.py \
+-file /home/uic/bd-project/task03/reducer0303.py -reducer /home/uic/bd-project/task03/reducer0303.py \
+-input /output/bd_project/task03/task0402/part-00000 -output /output/bd_project/task03/task0505/
+
